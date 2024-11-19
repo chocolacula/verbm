9e18f182585ba5b33b055634cc1d66d3eff4c3a8
Maxim Voloshin <mvolloshin@gmail.com>
2024-11-19 22:03:21 +0200

    add up and down commands

--- src/verbum/run.py
+++ src/verbum/run.py
@@ -6,91 +6,6 @@ from verbum.source import SourceManager
 
 VERSION = "0.0.0"
 
-"""
-class Context:
-    def __init__(self, filepath):
-        if filepath == None:
-            filepath = os.path.dirname(os.path.realpath(__file__))
-            filepath = os.path.join(filepath, DFILE)
-
-        if os.path.exists(filepath):
-            self.file = open(filepath, "r+")
-            self.data = json.load(self.file)
-        else:
-            self.data = {}
-            stderr.write("Manifest file doesn't exist\n")
-
-    def write_version(self, version: str):
-        self.data["version"] = version
-
-        self.file.seek(0)
-        self.file.truncate()
-        json.dump(self.data, self.file, indent=INDENT)
-        self.file.write("\n")  # trailing line
-        self.file.close()
-
-    def get_version(self) -> Optional[Tuple[int, int, int]]:
-        if len(self.data) == 0:
-            return None
-
-        if "version" in self.data:
-            return parse_version(self.data["version"])
-        else:
-            stderr.write("Cannot find 'version' field in the file\n")
-            return None
-
-
-
-    elif args.command == "up":
-        ver = ctx.get_version()
-
-        if ver == None:
-            return
-
-        major, minor, patch = ver
-
-        if args.component == "major":
-            major += 1
-            minor, patch = 0, 0
-        elif args.component == "minor":
-            minor += 1
-            patch = 0
-        elif args.component == "patch":
-            patch += 1
-
-        v = f"{major}.{minor}.{patch}"
-        ctx.write_version(v)
-
-        if args.push:
-            o = ".".join(map(str, ver))
-            ctx.push(f"version up from {o} to {v}", args.tag)
-
-    elif args.command == "down":
-        ver = ctx.get_version()
-
-        if ver == None:
-            return
-
-        major, minor, patch = ver
-
-        if args.component == "major":
-            major -= 1
-        elif args.component == "minor":
-            minor -= 1
-        elif args.component == "patch":
-            patch -= 1
-
-        v = f"{major}.{minor}.{patch}"
-        if major < 0 or minor < 0 or patch < 0:
-            stderr.write(f"A version component cannot be negative {v}\n")
-            return
-
-        ctx.write_version(v)
-        if args.push:
-            o = ".".join(map(str, ver))
-            ctx.push(f"version down from {o} to {v}", args.tag)
-"""
-
 
 def run():
     # parse command line arguments
@@ -118,10 +33,39 @@ def run():
         print(f"version {v} is consistent accors all files")
         exit(0)
 
+    # we are reade to check all other commands
+    v2 = copy.copy(v)
+    msg = ""
+
     if args.command == "set":
-        v2 = copy.copy(v)
         v2.parse(args.new_version)
 
-        source.replace(v, v2)
-        print(f"version was set to {v2}")
-        exit(0)
+        msg = f"version was set to {v2}"
+
+    elif args.command == "up":
+        if args.component == "major":
+            v2.major += 1
+            v2.minor, v2.patch = 0, 0
+        elif args.component == "minor":
+            v2.minor += 1
+            v2.patch = 0
+        elif args.component == "patch":
+            v2.patch += 1
+
+        msg = f"version bumped to {v2}"
+
+    elif args.command == "down":
+        if args.component == "major":
+            v2.major -= 1
+        elif args.component == "minor":
+            v2.minor -= 1
+        elif args.component == "patch":
+            v2.patch -= 1
+
+        if v2.major < 0 or v2.minor < 0 or v2.patch < 0:
+            raise Exception(f"version component cannot be negative {v2}")
+
+        msg = f"version downgraded to {v2}"
+
+    print(msg)
+    source.replace(v, v2)
